5e9d09581698bd3a3e1725a59d580976079d9979
Emery Goss (AIVAS-v2:Ubuntu-22.04) <m.goss792@gmail.com>
2025-05-03 02:24:27 +0000

    feat: add extractor shell

---
+++ src/mst/extractor.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass
+
+from pygit2 import Oid
+from pygit2.repository import Repository
+
+from mst.st_actions import StAction, StCommitMapping
+
+
+@dataclass
+class Extractor:
+    repo: Repository
+    mappings: dict[Oid, StCommitMapping]
+    actions: list[tuple[Oid, StAction]]
+
+    def extract(self) -> Oid:
+        for host_cid, action in self.actions:
+            # TODO
+            pass
+
+        last_host_cid = self.actions[-1][0]
+        return self.mappings[last_host_cid].subtree_commit_id
