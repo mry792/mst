f87a4da081d656a41445a94f849e89731fe012e4
Emery Goss (AIVAS-v2:Ubuntu-22.04) <m.goss792@gmail.com>
2025-05-03 02:24:46 +0000

    feat: add st_actions

---
+++ src/mst/st_actions.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+import yaml
+from pygit2 import Oid
+
+
+@dataclass(frozen=True)
+class StCommitMapping:
+    prefix: Path
+    subtree_commit_id: Oid
+
+
+@dataclass(frozen=True)
+class StNew:
+    prefix: Path
+
+
+@dataclass(frozen=True)
+class StMove:
+    new_prefix: Path
+
+
+type StAction = StCommitMapping | StNew | StMove | None
+
+
+class YamlParseTypeError(RuntimeError):
+    def __init__(self, data_type: type):
+        super().__init__(f"unexpected data type '{data_type}'")
+
+
+class YamlParseKeysError(RuntimeError):
+    def __init__(self, project_name: str, cid: Oid, keys: set[str]):
+        super().__init__(
+            f"Unexpected keys in note 'refs/notes/mst/{project_name}:"
+            f"{cid.hex}': {keys}",
+        )
+
+
+def parse_st_action(text: str, project_name: str, cid: Oid) -> StAction:
+    data = yaml.safe_load(text)
+    if not isinstance(data, dict):
+        raise YamlParseTypeError(type(data))
+
+    keys = set(data.keys())
+    if keys == {"add"}:
+        return StNew(Path(data["add"]))
+    if keys == {"move"}:
+        return StMove(Path(data["move"]))
+    if keys == {"prefix", "subtree_commit"}:
+        return StCommitMapping(
+            Path(data["prefix"]),
+            Oid(hex=data["subtree_commit"]),
+        )
+
+    raise YamlParseKeysError(project_name, cid, keys)
